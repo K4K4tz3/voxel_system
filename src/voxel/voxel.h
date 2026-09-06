aa94d43e0c78d3c255225a056272fd989951d35d
K4K4tz3 <wolff.joshua2002@icloud.com>
2026-09-06 22:28:30 +0200

    [fix]: manual fix for voxel's directional position return functions

--- src/voxel/voxel.h
+++ src/voxel/voxel.h
@@ -8,12 +8,14 @@ namespace godot {
 struct Voxel {
 
   Node3D *center;
-  Vector3 top() const { return center->get_position() + Vector3(0, 1, 0); }
-  Vector3 bottom() const { return center->get_position() + Vector3(0, -1, 0); }
-  Vector3 front() const { return center->get_position() + Vector3(1, 0, 0); }
-  Vector3 back() const { return center->get_position() + Vector3(-1, 0, 0); }
-  Vector3 left() const { return center->get_position() + Vector3(0, 0, 1); }
-  Vector3 right() const { return center->get_position() + Vector3(0, 0, -1); }
+  // WARN: Offset has to be max half the voxels size
+  // MANUALLY set!!!
+  Vector3 top() const { return center->get_position() + Vector3(0, 0.5, 0); }
+  Vector3 bot() const { return center->get_position() + Vector3(0, -0.5, 0); }
+  Vector3 front() const { return center->get_position() + Vector3(0.5, 0, 0); }
+  Vector3 back() const { return center->get_position() + Vector3(-0.5, 0, 0); }
+  Vector3 left() const { return center->get_position() + Vector3(0, 0, 0.5); }
+  Vector3 right() const { return center->get_position() + Vector3(0, 0, -0.5); }
 };
 
 }; // namespace godot
